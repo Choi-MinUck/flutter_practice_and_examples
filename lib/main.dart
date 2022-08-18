046f349709428e0b3647f2e56dfda7e2e61b7df4
Choi MinUck <21900741@handong.ac.kr>
2022-08-18 14:38:34 +0900

    CardDeckButton5

--- lib/main.dart
+++ lib/main.dart
@@ -282,3 +282,95 @@ Widget TextFieldExample1() {
     style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
   );
 }
+
+Widget CardDeckButton5(int cardCount, String cardContent, String cardName) {
+  String cardPath = 'assets/images/${cardName}.png';
+
+  return Stack(
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(top: 15, left: 15),
+        child: SizedBox(
+          width: 150,
+          height: 224,
+          child: Stack(
+            children: [
+              Ink.image(
+                  image: AssetImage(cardPath),
+                  child: InkWell(
+                    onTap: null,
+                  ),
+                  fit: BoxFit.fill),
+            ],
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 10, left: 10),
+        child: SizedBox(
+          width: 150,
+          height: 224,
+          child: Stack(
+            children: [
+              Ink.image(
+                  image: AssetImage(cardPath),
+                  child: InkWell(
+                    onTap: null,
+                  ),
+                  fit: BoxFit.fill),
+            ],
+          ),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 5, left: 5),
+        child: SizedBox(
+          width: 150,
+          height: 224,
+          child: Stack(
+            children: [
+              Ink.image(
+                  image: AssetImage(cardPath),
+                  child: InkWell(
+                    onTap: null,
+                  ),
+                  fit: BoxFit.fill),
+            ],
+          ),
+        ),
+      ),
+      SizedBox(
+        width: 150,
+        height: 224,
+        child: Stack(
+          children: [
+            Ink.image(
+                image: AssetImage(cardPath),
+                child: InkWell(
+                  onTap: () {},
+                ),
+                fit: BoxFit.fill),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                '${cardCount} 장',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                cardContent,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    height: 150 / 100,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
